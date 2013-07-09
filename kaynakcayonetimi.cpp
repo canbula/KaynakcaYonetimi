08d17e5a7ef57736ab5799c593e522de4f4bd3ad
Bora Canbula <bora.canbula@cbu.edu.tr>
2013-07-09 23:25:28 +0300

    Toolbar eklendi

--- kaynakcayonetimi.cpp
+++ kaynakcayonetimi.cpp
@@ -3,11 +3,29 @@
 KaynakcaYonetimi::KaynakcaYonetimi(const wxString& title)
 	: wxFrame(NULL,wxID_ANY,title,wxDefaultPosition,wxSize(800,600))
 {
+	wxImage::AddHandler(new wxPNGHandler);
+	wxBitmap toolbarLibrary(wxT("resource/toolbar/Library.png"),wxBITMAP_TYPE_PNG);
+	wxBitmap toolbarBook(wxT("resource/toolbar/Book.png"),wxBITMAP_TYPE_PNG);
+	wxBitmap toolbarArticle(wxT("resource/toolbar/Article.png"),wxBITMAP_TYPE_PNG);
+	wxBitmap toolbarDocument(wxT("resource/toolbar/Document.png"),wxBITMAP_TYPE_PNG);
+	wxBitmap toolbarFile(wxT("resource/toolbar/File.png"),wxBITMAP_TYPE_PNG);
+	wxBitmap toolbarList(wxT("resource/toolbar/List.png"),wxBITMAP_TYPE_PNG);
+	
 	menubar = new wxMenuBar;
 	file = new wxMenu;
 	file->Append(wxID_EXIT,wxT("&Kapat"));
 	menubar->Append(file,wxT("&Dosya"));
 	SetMenuBar(menubar);
+	
+	//Toolbar
+	wxToolBar *toolbar = CreateToolBar();
+	toolbar->AddTool(ID_TOOLBAR_LIBRARY, wxT("Kaynaklar"),  toolbarLibrary);
+	toolbar->AddTool(ID_TOOLBAR_BOOK,    wxT("Kitap"),    toolbarBook);
+	toolbar->AddTool(ID_TOOLBAR_ARTICLE, wxT("Makale"), toolbarArticle);
+	toolbar->AddTool(ID_TOOLBAR_DOCUMENT,wxT("Doküman"),toolbarDocument);
+	toolbar->AddTool(ID_TOOLBAR_FILE,    wxT("Dosya"),    toolbarFile);
+	toolbar->AddTool(ID_TOOLBAR_LIST,    wxT("Liste"),    toolbarList);
+	toolbar->Realize();
 	
 	statusbar = new wxStatusBar(this,wxID_ANY,wxST_SIZEGRIP);
 	this->SetStatusBar(statusbar);
--- kaynakcayonetimi.h
+++ kaynakcayonetimi.h
@@ -24,3 +24,10 @@ public:
 	wxMenu *file;
 	wxStatusBar *statusbar;
 };
+
+const int ID_TOOLBAR_LIBRARY = 701;
+const int ID_TOOLBAR_BOOK = 702;
+const int ID_TOOLBAR_ARTICLE = 703;
+const int ID_TOOLBAR_DOCUMENT = 704;
+const int ID_TOOLBAR_FILE = 705;
+const int ID_TOOLBAR_LIST = 706;
